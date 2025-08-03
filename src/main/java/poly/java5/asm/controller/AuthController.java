1d5314864b653c7d810777bd085a97a795a5e1e3
quocn1003 <quocn1003@gmail.com>
2025-08-04 04:24:26 +0700

    add HomeController

--- src/main/java/poly/java5/asm/controller/AuthController.java
+++ src/main/java/poly/java5/asm/controller/AuthController.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
-
 import jakarta.servlet.http.HttpSession;
 import poly.java5.asm.service.AuthService;
 
