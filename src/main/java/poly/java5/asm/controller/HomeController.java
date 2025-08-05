9390d871e624094d15ca42662c60ae47ebc41d02
quocn1003 <quocn1003@gmail.com>
2025-08-05 18:02:24 +0700

    add html

--- src/main/java/poly/java5/asm/controller/HomeController.java
+++ src/main/java/poly/java5/asm/controller/HomeController.java
@@ -1,13 +1,20 @@
 package poly.java5.asm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import poly.java5.asm.service.ProductService;
 
 @Controller
 public class HomeController {
+    @Autowired
+    ProductService productService;
 
     @GetMapping("/home/index")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("products", productService.findAll()); // lay tat cả sản phẩm
         return "home";
     }
 }
